a9323b4725d9965a6453c30b2893840d9944c09f
Warren Harper <warrenjharper@gmail.com>
2013-10-06 14:13:16 -0400

    Added git installation

--- post-install.sh
+++ post-install.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get update
-sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg firefox elementary-tweaks wingpanel-slim indicator-synapse  elementary-blue-theme elementary-champagne-theme elementary-colors-theme elementary-dark-theme elementary-harvey-theme elementary-lion-theme elementary-milk-theme elementary-plastico-theme elementary-whit-e-theme elementary-thirdparty-icons elementary-plank-themes elementary-wallpaper-collection ssh mysql-server terminator python-pip
+sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg firefox elementary-tweaks wingpanel-slim indicator-synapse  elementary-blue-theme elementary-champagne-theme elementary-colors-theme elementary-dark-theme elementary-harvey-theme elementary-lion-theme elementary-milk-theme elementary-plastico-theme elementary-whit-e-theme elementary-thirdparty-icons elementary-plank-themes elementary-wallpaper-collection ssh mysql-server terminator python-pip git git-core meld
 
 sudo apt-get install lamp-server^
 sudo apt-get install php5-gd php-xml-parser php5-intl smbclient curl libcurl3 php5-curl openjdk-6-jre openjdk-6-jdk
@@ -60,6 +60,11 @@ mv source-sans-pro source-sans-pro.zip
 unzip -o -j source-sans-pro.zip
 cp *.otf ~/.fonts
 fc-cache -f -v
+
+git config --global user.name "Warren Harper"
+git config --global user.email warrenjharper@gmail.com
+git config --global core.editor emacs
+git config --global merge.tool meld
 
 run_setup_program emacs "Download the following \ncolor-theme-wombat+ \ncolor-theme-sanityinc \ncolor-theme-solarized-light"
 run_setup_program "firefox http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/index.html" "Download Sql Developer" 
